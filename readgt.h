21288dd6b0f68221adcaa3df3b42b9d3349f1720
Claire Giselle Jennie Joseph <clairejoseph101@gmail.com>
2024-07-11 09:10:31 -0400

    Two files pertinent to comparing the data.
    These files are needed to compare the data as they along with the gettingosmdatacopy file would give the number of gtfs stops, osm stops that are bus stops and osm stops which contain certain latitudes. The intention is to have the gettingosmdatacopy file compare stops based on latitude and longitude and compare their distances using the vincenty script.

---
+++ readgt.h
@@ -0,0 +1,21 @@
+#include <list>
+#include <iostream> 
+#include <string>
+#ifndef READGT_H
+#define READGT_H
+
+struct Stop {
+     std::string stop_id;
+     std::string stop_name;
+     std::string stop_lat;
+     std::string stop_lon;
+     ///double stop_lat;
+     ////double stop_lon;
+
+
+};
+
+void readgtfs(std::list<Stop>& stops);
+/////////void readgtfs(std::list<std::string>& myli);
+
+#endif
