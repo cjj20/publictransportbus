7527cfb97a03d64d58ac53a787a7695f8594b7a6
cjj20 <clairejoseph101@gmail.com>
2024-07-12 04:23:13 -0400

    vincenty inverse header file needed for vincentyinversecopyy.cpp

---
+++ vinceinv.h
@@ -0,0 +1,15 @@
+#include <list>
+#include <iostream> 
+#include <string>
+#include <cmath>
+#ifndef VINCEINV_H
+#define VINCEINV_H
+
+
+
+double convertToRadians(double deg);
+double vincentydistt(std::string lon1, std::string lat1, double lon2, double lat2);
+
+
+
+#endif
