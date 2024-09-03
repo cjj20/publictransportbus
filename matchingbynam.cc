9258627d38527cc9ae9c05ebcf694cb5d0cd929b
cjj20 <clairejoseph101@gmail.com>
2024-09-03 12:47:01 -0400

    code matches by name, prints number of matches and non-matches

--- matchingbynam.cc
+++ matchingbynam.cc
@@ -1,15 +1,15 @@
 
 /*
-Copyright (c) 2001-2022
+
 
 
 This shows the simplest way to use the osm.pbf reader. It just counts the number of objects in the file.
 
 To build this file :
-g++ -O2 -o counter example_counter.cc -losmpbf -lprotobuf -lz
+g++ -O2 -o counter matchingnynam.cc listgtfs22.cpp -losmpbf -lprotobuf -lz
 
 To run it:
-./counter path_to_your_data.osm.pbf
+./counter path_to_your_data.osm.pbf stops.txt
 */
 
 #include "libosmpbfreader/osmpbfreader.h"
@@ -28,15 +28,28 @@ To run it:
 
 using namespace osmpbfreader;
 
-     
+
+// reuse maybe  std::map<std::string, std::string> Tagg;
+
+struct osmcoordntag {
+	std::map<std::string, std::string> Tagg; //TODO change type
+	// TODO remove or modify if needed std::map<std::string, std::string, std::less<>> Tagg;
+	// const Tagg taggg; //TODO change or replace maybe
+	double lati;
+	double loni;
+	
+	osmcoordntag(double lati, double loni, const std::map<std::string, std::string> &tags) : lati(lati), loni(loni), Tagg(tags) {}
+};
 
 
-    struct tagz {
+/*     struct tagz {
        // tags;
-    };
+    }; */
+	
     // std::string xtt;
-//gtfs stops
+	//gtfs stops
      std::list<Stop> stops;
+	 /*
     //  readgtfs(stops, argv[2]);
     ///  auto xtt = stops.stop_name;
      //// stops::stop_name& xtt = stops.stop_name;
@@ -50,9 +63,6 @@ using namespace osmpbfreader;
 */
 
 
-
-
-
 // We need to define a visitor with three methods that will be called while the file is read
 struct nameMatch {
     // Three integers count how many times each object type occurs
@@ -62,33 +72,32 @@ struct nameMatch {
 
 
     std::vector<Tags> tagss;
-    std::vector<tagz> taglisty;
+	std::vector<osmcoordntag> vlopsecondd;
+	
+	
+    // std::vector<tagz> taglisty;
 
     nameMatch() : nodes(0), ways(0), relations(0) {}
 
     // This method is called every time a Node is read
-    void node_callback(uint64_t /*osmid*/, double /*lon*/, double /*lat*/, const Tags &tags){
+    void node_callback(uint64_t /*osmid*/, double lon, double lat, const Tags &tags){
         //++nodes;
        // std::string xtt; //= gtfs.stop_name;
         auto nam = tags.find("name");
         auto highw = tags.find("highway");
        //// if(highw != tags.end() && highw->second == "bus_stop" && nam != tags.end() && nam->second==xtt) {
         if(highw != tags.end() && highw->second == "bus_stop") {
-        ++nodes;
-        tagss.push_back(tags);
-       /*
-         for (const auto& stop : stops) {
-          auto xttt = stop.stop_name;
-     std::cout << "gtfs stops names maybe" << xttt <<std::endl;
-     ++nodes;
-     }
-    */
-
-
-
-
-    }         
-        
+			++nodes;
+			tagss.push_back(tags);
+			vlopsecondd.push_back({lat, lon, tags}); //TODO may need to remove or change this line
+		   /*
+			 for (const auto& stop : stops) {
+			  auto xttt = stop.stop_name;
+			 std::cout << "gtfs stops names maybe" << xttt <<std::endl;
+			 ++nodes;
+			 }
+			*/
+		}                
     }
 
     // This method is called every time a Way is read
@@ -105,12 +114,9 @@ struct nameMatch {
 
 
 
-const std::vector<Tags>& getTagsList() const {
+	const std::vector<Tags>& getTagsList() const {
         return tagss;
     }
-
-
-
 
 };
 
@@ -167,17 +173,20 @@ struct Counter {
 
 
 int main(int argc, char** argv) {
-     if(argc != 2) {
-         std::cout << "Usage: " << argv[0] << " file_to_read.osm.pbf" << std::endl;
+     if(argc != 3) {
+         std::cout << "Usage: " << argv[0] << " file_to_read.osm.pbf stops.txt" << std::endl;
           ///readgtfs(stops, argv[2]);
         
          return 1;
      }
 
      std::vector<nameMatch> namematchhh;
-
+     nameMatch namematchin;
 
      readgtfs(stops, argv[2]);
+     int countingNameMatches = 0;
+	 int countingNonNameMatches = 0;
+	 
   /*    auto xtt = stops.stop_name;
     
      for (const auto& stop :stops) {
@@ -212,6 +221,8 @@ int main(int argc, char** argv) {
      std::cout << "We read for rd_osm_pbf" << countt.nodes << " nodes, " << countt.ways << " ways and " << countt.relations << " relations" << std::endl;
 
 */
+
+
       int chose;
       std::cout << "Choose whether to compare by name or by distance. Enter 1 for name and 2 for distance:  ";
       std::cin >> chose;
@@ -220,7 +231,7 @@ int main(int argc, char** argv) {
 
       switch (chose) {
         case 1: {
-            nameMatch namematchin;
+           //TODo reuse  nameMatch namematchin;
             readd_osm_pbf(argv[1], namematchin);
             std::cout << "We read for namematchin" << namematchin.nodes << " nodes, " << namematchin.ways << " ways and " << namematchin.relations << " relations" << std::endl;
             break;
@@ -249,17 +260,53 @@ int main(int argc, char** argv) {
 
 */
 
+    
+	
+	
+	
     for (const auto& stop : stops) {
-     std::cout << "gtfs stops names maybe" << stop.stop_name <<std::endl;
+	/* TODO REINSTATE std::cout << "gtfs stuff here" << std::endl;
+     std::cout << "gtfs stops names maybe" << stop.stop_name << std::endl; */
       // for(const auto& osmm :  namematchhh) {
-         for(const auto& osmm :  namematching.getTagsList()) {
+		 // TODO reuse  for(const auto& osmm : namematching.vlopsecondd) {
+	     for(const auto& osmm : namematchin.vlopsecondd) {
+			 
+         // TODO fix this   for(const auto& osmm :  namematching.getTagsList()) {
           //const char* namess = osmm.tags().get_value_by_key("name"); // osmm.tags()["name"]
-           auto nameTag = osmm.find("name");
-           const char* namess = (nameTag != osmm.end()) ? nameTag->second.c_str() : nullptr; // osmm.tags()["name"]
-           if(namess && !std::strcmp(namess, stop.stop_name.c_str())) {
-
-              std::cout << stop.stop_name << "and" << namess << std::endl;
-           }
+		  //ToDO REINSTATE std::cout << "Inside the for loop of vlopsecondd" << std::endl;
+		  //TODO REINSTATE std::cout << "test0" << std::endl;
+           auto nameTag = osmm.Tagg.find("name"); //TODO replace
+		 //TODO reuse  auto nameTagTwo = Tagg["name"]; //Tagg["name"];
+		   auto nameTagThree = osmm.Tagg.find("name");
+		//TODO REINSTATE   std::cout << "test1" << std::endl;
+		   
+          // const char* namess = (nameTag != osmm.end()) ? nameTag->second.c_str() : nullptr; // osmm.tags()["name"]
+		//TODO   const char* namess = (nameTag != osmm.Tagg.end()) ? nameTag->second.c_str() : nullptr;
+           std::string namess = (nameTagThree != osmm.Tagg.end()) ? nameTagThree->second : "";
+		   std::string namess2 = "A.Avenue & 10th Street";  //TODO! use as test
+		 //TODO REINSTATE  std::cout << "test2" << std::endl;
+		   //std::cout << nameTagThree->second << "second" << std::endl;
+		/* TODO REINSTATE  if (nameTagThree != osmm.Tagg.end()) {
+                // Output the value of nameTagThree
+				std::cout << "Key 'name' " << nameTagThree->first << std::endl;
+                std::cout << "Key 'name' found with value: " << nameTagThree->second << std::endl;
+				
+          *  } */
+		   
+		   //use std::string::npos
+		   //if(namess.find(stop.stop_name) != std::string::npos {
+		  //std::cout << "nameTag key: " << nameTagThree->first << " namess values: " << namess << std::endl;
+		  /// TODO reuse ?  if(namess && !std::strcmp(namess, stop.stop_name.c_str())) {
+		  if (!namess.empty() && namess == stop.stop_name) {
+              
+              std::cout << stop.stop_name << " and " << namess << std::endl;
+			  countingNameMatches++;  ///may need to remove 
+           } else {
+		   
+			// TODO REINSTATE   std::cout << namess << " no matching for osm name" << std::endl;
+			// TODO REINSTATE   std::cout << stop.stop_name << " no matching " << std::endl;
+			   countingNonNameMatches++; //may need to remove
+		   }
      }
      
     }
@@ -280,6 +327,7 @@ int main(int argc, char** argv) {
         }
 */
     
-
+    std::cout << "Number of name matches: " << countingNameMatches << std::endl;
+    std::cout << "Number of non-name matches: " << countingNonNameMatches << std::endl;
      return 0;
 }
